9dada5e1459a6cf5d596561cc20e1ac4b3ad882a
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2023-08-08 07:52:10 -0300

    CategoryDTO

--- backend/src/main/java/com/lorram/catalog/services/CategoryService.java
+++ backend/src/main/java/com/lorram/catalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.lorram.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lorram.catalog.dto.CategoryDTO;
 import com.lorram.catalog.entities.Category;
 import com.lorram.catalog.repositories.CategoryRepository;
 
@@ -14,8 +17,10 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
