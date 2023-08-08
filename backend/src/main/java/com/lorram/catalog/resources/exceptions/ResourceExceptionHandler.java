ded732e7ebdadb0f7744e720232689bbec7891c0
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2023-08-08 11:46:33 -0300

    Update and Delete Category, DatabaseException created

--- backend/src/main/java/com/lorram/catalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/lorram/catalog/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.lorram.catalog.services.exceptions.DatabaseException;
 import com.lorram.catalog.services.exceptions.ObjectNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,7 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), "Object not found", e.getMessage(), request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), "Database error", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
 	}
 	
 }
--- backend/src/main/java/com/lorram/catalog/services/CategoryService.java
+++ backend/src/main/java/com/lorram/catalog/services/CategoryService.java
@@ -13,6 +13,8 @@ import com.lorram.catalog.entities.Category;
 import com.lorram.catalog.repositories.CategoryRepository;
 import com.lorram.catalog.services.exceptions.ObjectNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class CategoryService {
 
@@ -32,12 +34,30 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 	
-	@Transactional(readOnly = true)
+	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		Category entity = repository.getReferenceById(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity); 
+		}
+		catch(EntityNotFoundException e) {
+			throw new ObjectNotFoundException(id);
+		}
+	}
+	
+	public void delete (Long id) {
+		findById(id);
+		repository.deleteById(id);
 	}
 	
 }
